fda259a1a275f1722a8b3f08114e6acd6446aba0
Ruben Baskaran <rbaskaran92@gmail.com>
2018-07-25 12:29:21 +0200

    Modified RegularExpressions

--- src/com/rubenbaskaran/RegularExpressions.java
+++ src/com/rubenbaskaran/RegularExpressions.java
@@ -5,8 +5,11 @@ class RegularExpressions
     static void RegularExpressions()
     {
         String message = "||hello_ world123!? :-)||";
+        System.out.println(message.replaceAll("[^a-zA-Z0-9\\s]", "")); // OUTPUT: hello world123
         System.out.println(message.replaceAll("[^a-zA-Z0-9]", "")); // OUTPUT: helloworld123
         System.out.println(message.replaceAll("[^a-zA-Z]", "")); // OUTPUT: helloworld
         System.out.println(message.replaceAll("[^0-9]", "")); // OUTPUT: 123
     }
 }
+
+
